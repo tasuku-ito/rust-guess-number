107e84b5f89c97742b0aba475c86f2d92b601d97
tasuku-ito <tasukuito5926@gmail.com>
2020-08-21 20:22:02 +0900

    Update end数当てゲーム

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
 extern crate rand;
 
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Guess the number!");
-    let secrete_number = rand::thread_rng().gen_range(1,101);
-    println!("Secret number is {}", secrete_number);
+    println!("Guess the number!"); // 数を当ててごらん
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
     loop {
-        println!("Please input your guess");
+        println!("Please input your guess."); // ほら、予想を入力してね
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // 行の読み込みに失敗しました
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Enter number!");
+                continue;
+            }
         };
-        println!("You guessed: {}", guess);
 
-        match guess.cmp(&secrete_number){
-            Ordering::Less => println!("Too smaell!"),
-            Ordering::Greater => println!("Too big!"),
+        println!("You guessed: {}", guess); // 次のように予想しました: {}
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
             Ordering::Equal => {
                 println!("You win!");
                 break;
-            },
+            }
+            Ordering::Greater => println!("too large"),
         }
     }
 }
